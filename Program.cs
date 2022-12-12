475dffad7e790ead9b564eae12c65864ca5f21c2
ahmad <115988600+AHMADDAWAGREH29@users.noreply.github.com>
2022-12-12 18:22:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ATM_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bankCapacity = 10;  //indicate bankcapacity
+            Person p1 = new Person();
+
+            string pinCode = "9876";   // pincode of 4 digits
+            string cardNumber = "12345789";
+            int accountBalance = 250;   // initinal balance
+            BankAccount tmpAccount = new BankAccount(p1, "Mohammaddawagreh@test.com", cardNumber, pinCode, accountBalance);
+
+
+            Bank testBank = new Bank(bankCapacity);
+
+            testBank.AddNewAccount(tmpAccount);
+
+            testBank.Save();
+
+            Bank newTestBank = new Bank();
+            newTestBank.Load();
+        }
+    }
+}
